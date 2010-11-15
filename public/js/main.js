60629e8c2532e7c9687d2757636fd1693b6a1454
Ryan Heimbuch <rheimbuch@gmail.com>
2010-11-15 14:13:20 -0700

    loading models for client side testing

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,17 @@ require(
   {
     baseUrl: "/lib/"
   },
-  ["model/document"],
-  function(documentModel){
+  [
+  "client/model/document",
+  "client/model/document_collection",
+  "js/form/dom-stream.js"
+  ],
+  function(documentModel, documentCollection){
     DocumentModel = documentModel;
+    DocumentCollection = documentCollection;
+    
+    sampleDocs = new DocumentCollection();
+    sampleDocs.url = "/documents/sample1";
+    sampleDocs.fetch();
   }
 )
